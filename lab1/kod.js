65d98575b9f7091818da77398a056ac4b41ecc8a
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-10-08 20:37:44 +0200

    obramowki

---
+++ lab1/kod.js
@@ -0,0 +1,14 @@
+function WYP() 
+{
+    document.getElementById("mail").value = "mail@gmail.com";
+    document.getElementById("poczta").value = "09-600";
+    document.getElementById("nip").value = "123-456-78-19";
+    document.getElementById("dowod").value = "QWE456456";
+    document.getElementById("ip4").value = "192.168.0.1";
+    document.getElementById("strona").value = "https://www.youtube.com/";
+    document.getElementById("dysk").value = 'C:\\windows\\system32';
+    document.getElementById("dysk2").value = 'C:\\wIndowS\\temp';
+    document.getElementById("etc").value = '/etc/passwd';
+    document.getElementById("ipv6").value = '2001:0db8:0:0:0:0:1428:57ab';
+    document.getElementById("telefon").value = '+48123456789'; 
+}
