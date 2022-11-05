70929eea873e0d79a336ea21b436cf560c770019
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-11-05 18:11:02 +0100

    start

---
+++ lab3/kod.js
@@ -0,0 +1,19 @@
+function WYP() 
+{
+    document.getElementById("mail").value = "mail@gmail.com";
+    document.getElementById("poczta").value = "09-600";
+    document.getElementById("nip").value = "123-456-78-19";
+    document.getElementById("dowod").value = "QWE456456";
+    document.getElementById("ip4").value = "192.168.0.1";
+    document.getElementById("strona").value = "https://www.youtube.com/";
+    document.getElementById("dysk").value = 'C:\\windows\\system32';
+    document.getElementById("dysk2").value = 'C:\\wIndowS\\temp';
+    document.getElementById("etc").value = '/etc/passwd';
+    document.getElementById("ipv6").value = '2001:0db8:0:0:0:0:1428:57ab';
+    document.getElementById("telefon").value = '+48123456789'; 
+document.getElementById("data").value = '2022-10-22';
+document.getElementById("roboczy").value = '2022-10-25';
+document.getElementById("godzina12").value = '07:45';
+document.getElementById("godzina24").value = '16:33';
+document.getElementById("color").value = '#12345';
+}
