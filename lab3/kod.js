3b78d370b395c8d01db2bda535972fe4dd3048ca
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-11-05 18:46:35 +0100

    Box start

--- lab3/kod.js
+++ lab3/kod.js
@@ -35,3 +35,17 @@ let list;
       }
     }
   });
+
+
+function adddiv(n) {
+  for(i=0;i<n;i++){$("<div id='box' style='background:"+getRandomColor()+"';></div>").hide().appendTo("#wrapper").fadeIn(150);}
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
