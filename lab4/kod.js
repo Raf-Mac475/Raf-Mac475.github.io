46454c8a9513d3c82dfa3359c8b5cf7da4c91db1
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-11-19 18:45:50 +0100

    db_update

--- lab4/kod.js
+++ lab4/kod.js
@@ -1,12 +1,8 @@
 let db;
-let request = window.indexedDB.open("newDatabase", 1);
-request.onerror = function (event) 
-	{
-        console.log("error: The database is opened failed");
-      	};
-request.onsuccess = function (event) 
-	{
-        db = request.result;
-        console.log("success: The database " + db + " is opened successfully");
-        drawTable();
-      	};
+let dbReq = indexedDB.open("newDatabase", 1);
+
+dbReq.onupgradeneeded = function (event) {
+    db = event.target.result;
+
+    let clients = db.createObjectStore('clients', { autoIncrement: true });
+}
