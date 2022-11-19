61a683ffc928cb611e9ed6e9b078947becfd0877
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-11-19 18:56:20 +0100

    Update kod.js

--- lab4/kod.js
+++ lab4/kod.js
@@ -1,8 +1,31 @@
 let db;
-let dbReq = indexedDB.open("newDatabase", 1);
+      let request = window.indexedDB.open("newDatabase", 1);
 
-dbReq.onupgradeneeded = function (event) {
-    db = event.target.result;
+      request.onerror = function (event) {
+        console.log("error: The database is opened failed");
+      };
 
-    let clients = db.createObjectStore('clients', { autoIncrement: true });
-}
+      request.onsuccess = function (event) {
+        db = request.result;
+        console.log("success: The database " + db + " is opened successfully");
+        drawTable();
+      };
+
+      request.onupgradeneeded = function (event) {
+        var db = event.target.result;
+        var objectStore = db.createObjectStore("client", {
+          autoIncrement: true,
+        });
+
+        objectStore.createIndex("name", "name", { unique: false });
+        objectStore.createIndex("lastName", "lastName", { unique: false });
+        objectStore.createIndex("age", "age", { unique: false });
+        objectStore.createIndex("email", "email", { unique: true });
+        objectStore.createIndex("pesel", "pesel", { unique: true });
+        objectStore.createIndex("address", "address", { unique: false });
+        objectStore.createIndex("phoneNumber", "phoneNumber", { unique: true });
+
+        for (var i in clientData) {
+          objectStore.add(clientData[i]);
+        }
+      };
