7ab170e38c8ca183c6cd5cb34621ceceffb8b7d8
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-11-19 19:02:56 +0100

    update

--- lab4/kod.js
+++ lab4/kod.js
@@ -1,3 +1,25 @@
+if (!window.indexedDB) {
+        window.alert(
+          "Your browser doesn't support a stable version of IndexedDB."
+        );
+      }
+
+      const clientData = [
+        {
+          name: "Rafał",
+          lastName: "Mackiewicz",
+          age: "20",
+          email: "Rafal@gmail.com",
+        },
+        {
+          name: "Patryk",
+          lastName: "Kowalski",
+          age: "30",
+          email: "Patryk@gmail.com",
+        },
+      ];
+
+
 let db;
       let request = window.indexedDB.open("newDatabase", 1);
 
@@ -21,11 +43,35 @@ let db;
         objectStore.createIndex("lastName", "lastName", { unique: false });
         objectStore.createIndex("age", "age", { unique: false });
         objectStore.createIndex("email", "email", { unique: true });
-        objectStore.createIndex("pesel", "pesel", { unique: true });
-        objectStore.createIndex("address", "address", { unique: false });
-        objectStore.createIndex("phoneNumber", "phoneNumber", { unique: true });
 
         for (var i in clientData) {
           objectStore.add(clientData[i]);
         }
       };
+
+function add(event) {
+        event.preventDefault();
+
+        var formElements = document.getElementById("addForm");
+
+        var request = db
+          .transaction(["client"], "readwrite")
+          .objectStore("client")
+          .add({
+            name: formElements[0].value,
+            lastName: formElements[1].value,
+            age: formElements[2].value,
+            email: formElements[3].value,
+          });
+
+        request.onsuccess = function (event) {
+          console.log("Client added");
+          drawTable();
+        };
+
+        request.onerror = function (event) {
+          alert(
+            "Unable to add data\r\ user with that email aready exist in your database! "
+          );
+        };
+      }
